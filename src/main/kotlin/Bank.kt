1c19e0ab073be42d8a325c90e746323354b6c5ff
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-03 06:55:40 +0200

    feat: add Bank class with deposit, withdraw, and balance retrieval methods

---
+++ src/main/kotlin/Bank.kt
@@ -0,0 +1,14 @@
+﻿class Bank {
+    private var balance: Double = 0.0
+    fun deposit(amount: Double) {
+        balance += amount
+    }
+
+    fun withdraw(amount: Double) {
+        balance -= amount
+    }
+    fun getBalance(): Double {
+        return balance
+    }
+
+}
